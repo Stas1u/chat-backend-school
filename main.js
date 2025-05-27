7bd96cce80c8c3955b7e0e65d76b59be80af23ec
Stas1u <kaonners@gmail.com>
2025-05-28 00:32:09 +0200

    database init

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const express = require("express")
+const sqlite3 = require("sqlite3").verbose()
 
 const app = express()
+const db = new sqlite3.Database("./data.sqlite")
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+
+const all_users = () => 
+    new Promise((resolve, reject) => {
+        db.all("SELECT * FROM app_users", (err, rows) => {
+            if (err) reject(err)
+            else resolve(rows)
+        })
+    })
+
+app.get("/", async (req, res) => {
+    const data = await all_users()
+    // console.log(data)
+    res.send(data)
 })
 
 app.listen(8000, () => console.log("Server is listening at port 8000"))
